a052caae358cbbd4da7108f5cbbcdbe16a07063b
Jorge Cadena <a01660252@tec.mx>
2024-05-18 15:52:08 -0600

    Created the df with the whole 'mexican_trees.csv' database, created a regions df with only the sates and created the regions list.

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
-print('Hello World!')
+import pandas as pd
+# import math
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# from itertools import combinations
+# from thefuzz import fuzz, process
 
-print('Hello Jorge')
+# Get the df of the mexican trees
+mexicanTreesDf = pd.read_csv('mexican_trees.csv')
+print(mexicanTreesDf.head())
+
+# # Count how many Species there are
+# species = []
+# for specie in mexicanTreesDf['SPECIES']:
+#     if specie not in species:
+#         species.append(specie)
+#
+# print(len(species))
+
+# Check how many states are in the db
+states = []
+for state in mexicanTreesDf['STATE']:
+    if state not in states:
+        states.append(state)
+print(states)
+
+# Create a regions dataframe
+regionsDf = pd.DataFrame(states, columns=['STATE'])
+
+# Create regions
+northWestRegion = ['Baja California', 'Baja California Sur', 'Chihuahua',
+                   'Durango', 'Sinaloa', 'Sonora']
+northEast = ['Coahuila', 'Nuevo León', 'Tamaulipas']
+west = ['Colima', 'Jalisco', 'Michoacán', 'Nayarit']
+east = ['Hidalgo', 'Puebla', 'Tlaxcala', 'Veracruz']
+northCenter = ['Aguascalientes', 'Guanajuato', 'Querétaro', 'San Luis Potosí',
+               'Zacatecas']
+southCenter = ['Ciudad de México', 'México', 'Morelos']
+southWest = ['Chiapas',  'Guerrero', 'Oaxaca']
+southEast = ['Campeche', 'Quintana Roo', 'Tabasco', 'Yucatán']
+
