291f8f49d8cf1f1d5fff3879d9311140b7ba0415
Jorge Cadena <a01660252@tec.mx>
2024-05-18 17:50:21 -0600

    Created the regionsDf, which contains the states and the region they belong to

--- main.py
+++ main.py
@@ -8,35 +8,45 @@ import pandas as pd
 
 # Get the df of the mexican trees
 mexicanTreesDf = pd.read_csv('mexican_trees.csv')
-print(mexicanTreesDf.head())
-
-# # Count how many Species there are
-# species = []
-# for specie in mexicanTreesDf['SPECIES']:
-#     if specie not in species:
-#         species.append(specie)
-#
-# print(len(species))
 
 # Check how many states are in the db
 states = []
 for state in mexicanTreesDf['STATE']:
     if state not in states:
-        states.append(state)
-print(states)
+        states.append(state) # Store the states in states list
 
-# Create a regions dataframe
-regionsDf = pd.DataFrame(states, columns=['STATE'])
+# Create regions dictionary
+regionsDict = {
+    'Northwest': ['Baja California', 'Baja California Sur', 'Chihuahua',
+                   'Durango', 'Sinaloa', 'Sonora'],
+    'Northeast': ['Coahuila', 'Nuevo León', 'Tamaulipas'],
+    'West': ['Colima', 'Jalisco', 'Michoacán', 'Nayarit'],
+    'East': ['Hidalgo', 'Puebla', 'Tlaxcala', 'Veracruz'],
+    'Northcenter': ['Aguascalientes', 'Guanajuato', 'Querétaro', 'San Luis Potosí',
+               'Zacatecas'],
+    'Southcenter': ['Ciudad de México', 'México', 'Morelos'],
+    'Southwest': ['Chiapas',  'Guerrero', 'Oaxaca'],
+    'Southeast': ['Campeche', 'Quintana Roo', 'Tabasco', 'Yucatán']
+}
 
-# Create regions
-northWestRegion = ['Baja California', 'Baja California Sur', 'Chihuahua',
-                   'Durango', 'Sinaloa', 'Sonora']
-northEast = ['Coahuila', 'Nuevo León', 'Tamaulipas']
-west = ['Colima', 'Jalisco', 'Michoacán', 'Nayarit']
-east = ['Hidalgo', 'Puebla', 'Tlaxcala', 'Veracruz']
-northCenter = ['Aguascalientes', 'Guanajuato', 'Querétaro', 'San Luis Potosí',
-               'Zacatecas']
-southCenter = ['Ciudad de México', 'México', 'Morelos']
-southWest = ['Chiapas',  'Guerrero', 'Oaxaca']
-southEast = ['Campeche', 'Quintana Roo', 'Tabasco', 'Yucatán']
+# Create the regions list to store every state region
+regions = []
 
+# Iterate over the states to find their region
+for state in states:
+    foundRegion = None
+
+    for region, statesList in regionsDict.items():
+        if state in statesList:
+            foundRegion = region
+            break
+
+    regions.append(foundRegion) # Store the region
+
+# Create a df of only the states
+onlyStatesDf = pd.DataFrame(states, columns=['STATE'])
+# Create a df of only the regions
+onlyRegionsDf = pd.DataFrame(regions, columns=['REGION'])
+
+regionsDf = pd.concat([onlyStatesDf, onlyRegionsDf], axis=1)
+print(regionsDf)
