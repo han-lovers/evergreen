1514f0d01ae319935b3aed66d321b731e2b12866
leopalatto20 <A01665462@tec.mx>
2024-05-18 12:31:12 -0600

    ya funciona bien pero falta arreglar errores

--- DistanciaEntrePuntos.py
+++ DistanciaEntrePuntos.py
@@ -5,27 +5,38 @@ from geopy.distance import geodesic
 # Inicializar geocodificador
 geolocator = Nominatim(user_agent="pruebas")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
+reverse = RateLimiter(geolocator.reverse, min_delay_seconds=2)
 
 # Coordenadas de cada lugar
-location1_name = "Escuela Militar de Clases de Transmisiónes"
-location2_name = "Olivos Residencial"
+location1_name = input("Ingresa tu dirección o deja en blanco para usar coordenadas: ")
 
-location1 = geolocator.geocode(location1_name)
-location2 = geolocator.geocode(location2_name)
+if location1_name:
+    try:
+        location1 = geolocator.geocode(location1_name)
+        if not location1:
+            raise ValueError("Ubicación no encontrada.")
+    except Exception as e:
+        print(f"Error: {e}")
+else:
+    try:
+        location1_coords = input("Ingresa las coordenadas de tu ubicación (lat, long): ")
+        location1 = geolocator.reverse(location1_coords)
+        if not location1:
+            raise ValueError("Ubicación no encontrada.")
+    except Exception as e:
+        print(f"Error: {e}")
 
-# Verificar que las ubicaciones se encontraron
-if location1 is None:
-    print(f"No se pudo encontrar la ubicación para: {location1_name}")
-if location2 is None:
-    print(f"No se pudo encontrar la ubicación para: {location2_name}")
+location2 = "19.43257954644052, -99.13313474475538"
 
-if location1 and location2:
-    # Crear tuplas de coordenadas en el formato correcto (latitud, longitud)
-    location1Coords = (location1.latitude, location1.longitude)
-    location2Coords = (location2.latitude, location2.longitude)
+try:
+    # Extraer las coordenadas de location1
+    location1_coords = (location1.latitude, location1.longitude)
+
+    # Convertir location2 a coordenadas
+    location2_coords = tuple(map(float, location2.split(", ")))
 
     # Calcular y mostrar la distancia
-    distancia = geodesic(location1Coords, location2Coords).kilometers
-    print(f"La distancia entre {location1_name} y {location2_name} es de {distancia:.2f} kilómetros.")
-else:
-    print("No se pudo calcular la distancia debido a ubicaciones no encontradas.")
+    distancia = geodesic(location1_coords, location2_coords).kilometers
+    print(f"La distancia entre las ubicaciones es de {distancia:.2f} kilómetros.")
+except Exception as e:
+    print(f"Error al calcular la distancia: {e}")
